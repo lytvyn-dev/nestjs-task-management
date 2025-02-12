115e799521dcebd36abd32b7ff7ca045e283ab7b
Dmytro Lytvyn <111250341+lytvyn-dev@users.noreply.github.com>
2025-02-12 20:33:23 +0200

    feat: added validation to getTasksFilterDto

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,13 @@
+import { IsNotEmpty, IsOptional, IsEnum, IsString } from 'class-validator';
 import { TaskStatus } from "../task.model";
 
 export class GetTasksFilterDto {
+	@IsOptional()
+	@IsEnum(TaskStatus)
 	status?: TaskStatus;
+
+	@IsOptional()
+	@IsString()
+	@IsNotEmpty()
 	search?: string;
 }
